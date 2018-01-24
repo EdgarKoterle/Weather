7f8448edccc2a56f4e158ab3b691bf4f48c7337c
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-01-24 09:41:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function()
+{
+	$("#get").click(function()
+	{
+		var city=$("#city").val();
+		var code=$("#countryCode").val();
+		if (city.length>1)
+		{
+			var urlLink='http://api.openweathermap.org/data/2.5/weather?q=';
+			urlLink=urlLink+city;
+
+			if (code.length==2)
+				urlLink=urlLink+','+code;
+
+			urlLink=urlLink+'&appid=dd3d003b072c254def863bf7b0ba0b74';
+			$.ajax
+			{
+
+			}
+		}
+	});
+});
