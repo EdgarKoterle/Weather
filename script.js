35c621e035eb6297ce6e6b62de38d859ca70f8bb
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-01-25 10:05:13 +0100

    Added details table

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function()
 		var code=$("#countryCode").val();
 		if (city.length>1)
 		{
-			var urlLink='http://api.openweathermap.org/data/2.5/weather?q=';
+			var urlLink='https://api.openweathermap.org/data/2.5/weather?q=';
 			urlLink=urlLink+city;
 
 			if (code.length==2)
@@ -28,9 +28,63 @@ $(document).ready(function()
 				{
 					console.log("temp: "+data.main.temp);
 					console.log("desc: "+data.weather[0].description);
+
+					$('details1').empty();
+                	var table=$('<table/>');
+                	var tr=getLine('City:', city);
+                	table.append(tr);
+                	$('#mainTable').append(table);
+                
+                	var tr=getLine('Country: ', $('#code').val());
+                	table.append(tr);
+                
+                	var tr=getLine('Temperature:', parseFloat(data.main.temp-273.15).toFixed(1)+" ℃");
+                	table.append(tr);
+                
+                	var tr=getLine('Humidity:', data.main.humidity+" %");
+                	table.append(tr);
+                
+                	var tr=getLine('Description:', data.weather[0].description);
+                	table.append(tr);
+                
+                	var tr=getLine('Pressure:', data.main.pressure+' hPa');
+                	table.append(tr);
+                
+                	if ($("#details").is(":checked") == true)
+                	{
+                    	var tr=getLine('Sunrise:', data.sys.sunrise);
+                    	table.append(tr);
+                    
+                    	var tr=getLine('Sunset:', data.sys.sunset);
+                    	table.append(tr);
+                    
+                    	var tr=getLine('Wind:', data.wind.speed+' m/s');
+                    	table.append(tr); 
+                    
+                    	var tr=getLine('Min. temperature:', parseFloat(data.main.temp_min-273.15).toFixed(1)+" ℃");
+                    	table.append(tr);
+                    
+                    	var tr=getLine('Max. temperature:', parseFloat(data.main.temp_max-273.15).toFixed(1)+" ℃");
+                    	table.append(tr);
+                    
+                    	var tr=getLine('Min. temperature:', data.visibility);
+                    	table.append(tr);
+					};
 				},
 				type: 'GET'
 			});
 		}
 	});
+
+	function getLine (data1, data2)
+	{
+		var tr=$("<tr/>");
+		var td1=$("<td/>");
+		$(td1).append(data1);
+		var td2=$("<td/>");
+		$(td2).append(data2);
+		tr.append(td1);
+		tr.append(td2);
+		return tr;
+	}
 });
